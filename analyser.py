6a3ec8db2684f407998bae679510aefb11c9001c
James Archer <james__archer@hotmail.com>
2018-01-29 12:51:16 +1100

    Analyser more complete

--- analyser.py
+++ analyser.py
@@ -8,6 +8,7 @@ Created on Mon Jan 29 11:04:52 2018
 import numpy as np
 from matplotlib import pyplot as plt
 import os as os
+from sep_funcs import *
 
 class Analyser():
     '''
@@ -21,6 +22,11 @@ class Analyser():
         logFileName -- String. The name of the log file to be read. Standard format is just the file name (no ext).
             Can also be the absolute path (with ext).
         '''
+        
+        self.methods = {'SubtractBackground': SubtractBackground,
+                        'Sum': Sum
+                        }
+        
         if parentDir:
             os.chdir(parentDir)
         self.files = []
@@ -62,11 +68,12 @@ class Analyser():
         self.results = []
         for file in self.files:
             self.results.append(self.RunFile('./output/' + file))
-        plt.show()
+        self.results = np.array(self.results)
         
         if self.is2D:
             
             self.pos = np.array(np.meshgrid(np.arange(nx)*xStep+xStart, np.arange(ny)*yStep+yStart))
+            self.results = self.results.reshape(self.pos[0].shape)
             
         else:
         
@@ -75,4 +82,23 @@ class Analyser():
     def RunFile(self, fName):
         
         x = np.loadtxt(fName, dtype = np.float16, skiprows=4, delimiter='\t', usecols = 1)
+        print(x.min(), x.max())
+        x = self.methods['SubtractBackground'](x)
+        return self.methods['Sum'](x)
+    
+    def Plot3D(self):
         
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+        from matplotlib import cm
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(self.pos[0], self.pos[1], self.results, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+        plt.show()
+        
+    
+if __name__=='__main__':
+    
+    a = Analyser('07-01-2018-131122-LogFile', parentDir=r'C:\Users\jia335\Documents\PS_Waveform_Averager')
+    a.Plot3D()
